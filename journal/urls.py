4e917339c91909f62f31da25c367f2b0fe77939a
Will Griffiths <115983619+Will-Griffiths-Ireland@users.noreply.github.com>
2023-06-27 12:13:55 +0000

    Create - Journal Removal Page & 403 Page

--- journal/urls.py
+++ journal/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddJournalPage, Journals, ViewJournalPage
+from .views import AddJournalPage, Journals, ViewJournalPage, RemovePage
 
 
 urlpatterns = [
     path("", AddJournalPage.as_view(), name="add_journal_page"),
     path("journals/", Journals.as_view(), name="journals"),
     path("<slug:pk>/", ViewJournalPage.as_view(), name="view_journal_page"),
+    path("delete/<slug:pk>/", RemovePage.as_view(), name="remove_journal_page"),
 ]
--- journal/views.py
+++ journal/views.py
@@ -1,7 +1,8 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, DeleteView
 from .models import Journal
+import datetime
 from .forms import JournalForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class Journals(ListView):
@@ -29,11 +30,23 @@ class AddJournalPage(LoginRequiredMixin, CreateView):
     Add a page to the journal
     """
 
+    current_date = "MODNAYT"
+
     template_name = "journal/add_page.html"
     model = Journal
     form_class = JournalForm
-    success_url = "journals/"
+    success_url = "/journal/journals/"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(AddJournalPage, self).form_valid(form)
+
+
+class RemovePage(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """Remove Journal Page"""
+
+    model = Journal
+    success_url = "/journal/journals/"
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
