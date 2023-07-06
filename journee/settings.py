f1969a7685e7ca89063de2371397affb43c047f6
Will Griffiths <115983619+Will-Griffiths-Ireland@users.noreply.github.com>
2023-07-06 13:31:50 +0000

    Modify - Remove staticfiles_dir

--- journee/settings.py
+++ journee/settings.py
@@ -197,7 +197,7 @@ LOGIN_REDIRECT_URL = "/"
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
 
