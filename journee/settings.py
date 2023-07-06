51e7091dc0c9271341042b026039cfdca0e70a25
Will Griffiths <115983619+Will-Griffiths-Ireland@users.noreply.github.com>
2023-07-06 14:10:51 +0000

    Modify - Settings staticfiles_storage path

--- journee/settings.py
+++ journee/settings.py
@@ -195,9 +195,10 @@ LOGIN_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATIC_URL = "/static/"
 
 
 
