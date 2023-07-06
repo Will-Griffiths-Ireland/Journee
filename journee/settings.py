238add68e37220d0e63a046cf64d4975839788f9
Will Griffiths <115983619+Will-Griffiths-Ireland@users.noreply.github.com>
2023-07-06 13:29:36 +0000

    Modify - fixing static path

--- journee/settings.py
+++ journee/settings.py
@@ -196,7 +196,7 @@ LOGIN_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 
