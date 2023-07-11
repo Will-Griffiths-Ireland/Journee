dd860218e8c853885428245e0776efd1c3c9e4a0
Will Griffiths <115983619+Will-Griffiths-Ireland@users.noreply.github.com>
2023-07-11 20:48:32 +0000

    Modify - Journal Page Stylings

--- journal/models.py
+++ journal/models.py
@@ -38,16 +38,16 @@ class Journal(models.Model):
 
     def journalPageExistsForToday(self):
         """Check to see if there is already a page for this user today"""
-        today = datetime.date.today()
-        requestor = get_current_user()
-        journal_page = Journal.objects.filter(
-            journal_date=today,
-            user=requestor
-            )
-        if journal_page:
-            return journal_page[0].id
-        else:
-            return False
+        # today = datetime.date.today()
+        # requestor = get_current_user()
+        # journal_page = Journal.objects.filter(
+        #     journal_date=today,
+        #     user=requestor
+        #     )
+        # if journal_page:
+        #     return journal_page[0].id
+        # else:
+        return False
 
     def userHasFullProfile(self):
         """Check to see if this user has a full profile"""
@@ -59,7 +59,6 @@ class Journal(models.Model):
             return profile_match[0].user_id
         else:
             return False
-
 
     def addPageView(self):
         """
